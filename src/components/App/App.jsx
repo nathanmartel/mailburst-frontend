eb8f366639618750602dd03f8ae2e4481e94f65c
Nathan Martel <nathanmartel@gmail.com>
2020-07-27 16:10:07 -0700

    Add Bootstrap and Router essentials

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
-export default function App() {
-  return <h1>Hello World</h1>;
-}
-  
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+
+const App = () => (
+  <BrowserRouter>
+    <Container className="p-3">
+      <Jumbotron>
+        <h1 className="header">Welcome To MailBurst</h1>
+      </Jumbotron>
+    </Container>
+    <Switch>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default App;
